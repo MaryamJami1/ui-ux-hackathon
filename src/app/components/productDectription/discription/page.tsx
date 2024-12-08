33a7198a782df3f30e9c448c90b2dca3631eae87
MaryamJami1 <maryamchouhan373@gmail.com>
2024-12-08 21:46:47 +0500

    complete

--- src/app/components/productDectription/discription/page.tsx
+++ src/app/components/productDectription/discription/page.tsx
@@ -3,7 +3,7 @@ import FeaturedProducts from "../featureProduct"
 
 export default function ProductDescription() {
     return (
-        <div className="container mx-auto px-4 py-11 lg:px-20 ">
+        <div className="container mx-auto px-4 py-11 lg:px-[10rem] ">
             <div className="grid md:grid-cols-2 gap-8 items-start">
                 <div className="relative aspect-square">
                     <Image
